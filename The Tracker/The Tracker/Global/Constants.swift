3ee5a88f334f663c6371632f096955601003fc6f
kwokpingl <kwokpinglau.119@gmail.com>
2018-02-25 22:50:00 +0800

    Jimmy Updated (RecordModel, RecordViewModel, Setting Storyboard)

---
+++ The Tracker/The Tracker/Global/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  The Tracker
+//
+//  Created by Jimmy on 2018/2/24.
+//  Copyright © 2018年 WSJ. All rights reserved.
+//
+
+import Foundation
---
+++ The Tracker/The Tracker/Jimmy/ViewModel/RecordViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SearchViewModel.swift
+//  The Tracker
+//
+//  Created by Jimmy on 2018/2/24.
+//  Copyright © 2018年 WSJ. All rights reserved.
+//
+
+import Foundation
